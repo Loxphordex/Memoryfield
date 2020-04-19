6efdd81727413db943da40abbdc9d72ca4e58ba9
Silas Hallahan <silasishallahan@gmail.com>
2020-04-18 17:37:25 -0700

    Updates

---
+++ precache-manifest.5c926ce35e267261adb7b3bdee7b7cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493c881420e145f53ae6474749fffa69",
+    "url": "/Memoryfield/index.html"
+  },
+  {
+    "revision": "457cd4c9300057b8eb9c",
+    "url": "/Memoryfield/static/css/main.87267784.chunk.css"
+  },
+  {
+    "revision": "128d54cf2b0c32dab772",
+    "url": "/Memoryfield/static/js/2.805c606f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Memoryfield/static/js/2.805c606f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457cd4c9300057b8eb9c",
+    "url": "/Memoryfield/static/js/main.2f8d7b57.chunk.js"
+  },
+  {
+    "revision": "93b8b32cef4cfd085ea4",
+    "url": "/Memoryfield/static/js/runtime-main.f5fbc8eb.js"
+  }
+]);
