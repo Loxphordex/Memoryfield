247597d03aa62ffd32a110971b7d3897bc9d2887
Silas Hallahan <silasishallahan@gmail.com>
2020-04-22 11:36:26 -0700

    Updates

---
+++ precache-manifest.6f0cc0d789e828d8bf784f86f72d87bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a5fdeec2dd7fa229fbc53ee398481e",
+    "url": "/MemoryField/index.html"
+  },
+  {
+    "revision": "1b47e7e709550db636cc",
+    "url": "/MemoryField/static/css/main.87267784.chunk.css"
+  },
+  {
+    "revision": "128d54cf2b0c32dab772",
+    "url": "/MemoryField/static/js/2.805c606f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MemoryField/static/js/2.805c606f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b47e7e709550db636cc",
+    "url": "/MemoryField/static/js/main.2a0417ac.chunk.js"
+  },
+  {
+    "revision": "187f58345666b72956ca",
+    "url": "/MemoryField/static/js/runtime-main.fd5c8786.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.577052f1672122e2fd69cf85d9006546.js"
+  "/MemoryField/precache-manifest.6f0cc0d789e828d8bf784f86f72d87bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MemoryField/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
