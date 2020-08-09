c352936bd87b4d1f889263ef6a01ba00d861c91d
Silas <silasishallahan@gmail.com>
2020-08-08 22:39:56 -0700

    successful load

--- src/components/Presets/LoadPreset/LoadPreset.js
+++ src/components/Presets/LoadPreset/LoadPreset.js
@@ -8,6 +8,9 @@ export default function LoadPreset({
   setPresets
 }) {
   const [isMenuShowing, setIsMenuShowing] = useState(false)
+  const [isConfirmationShowing, setIsConfirmationShowing] = useState(false)
+  const [presetToLoad, setPresetToLoad] = useState(null)
+  const [nameOfPreset, setNameOfPreset] = useState(null)
   const toggleShowMenu = () => setIsMenuShowing(!isMenuShowing)
 
   useEffect(() => {
@@ -17,10 +20,57 @@ export default function LoadPreset({
     }
   }, [setPresets])
 
+  function presetItems() {
+    let items = []
+    if (presets) {
+      for (const [key] of Object.entries(presets)) {
+        items.push(key)
+      }
+    }
+    return items
+  }
+
+  function loadPreset(nodes) {
+    if (nodes) {
+      setNodes(nodes)
+      toggleShowMenu()
+    }
+  }
+
+  function confirmLoad() {
+    if (presetToLoad && nameOfPreset) {
+      return (
+        <div className='confirm-load'>
+          <h3>{`Load ${nameOfPreset}?`}</h3>
+          <button onClick={() => loadPreset(presetToLoad)}>Yes</button>
+          <button onClick={() => setIsConfirmationShowing(false)}>No</button>
+        </div>
+      )
+    }
+  }
+
+  function setupConfirmation(name) {
+    if (name) {
+      setNameOfPreset(name)
+      setPresetToLoad(presets[name])
+      setIsConfirmationShowing(true)
+    }
+  }
+
+  // create a function that displays available presets created by the user
+  // Once selected, the menu will disappear, a confirmation dialogue will appear
+  // Upon confirmation, the dialogue will disappear, and the new preset will be set
+
   return (
     <div className='load-preset'>
       <button onClick={toggleShowMenu}>Load</button>
-      {isMenuShowing && <LoadPresetMenu presets={presets} />}
+      {isMenuShowing && <LoadPresetMenu
+        presetItems={presetItems}
+        toggleShowMenu={toggleShowMenu}
+        isConfirmationShowing={isConfirmationShowing}
+        setupConfirmation={setupConfirmation}
+        confirmLoad={confirmLoad}
+      />}
     </div>
   )
 }
--- src/components/Presets/LoadPreset/LoadPresetMenu.js
+++ src/components/Presets/LoadPreset/LoadPresetMenu.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
-export default function LoadPresetMenu({ presets }) {
+export default function LoadPresetMenu({
+  presetItems,
+  toggleShowMenu,
+  isConfirmationShowing,
+  setupConfirmation,
+  confirmLoad
+}) {
   return (
     <div className='load-preset-menu'>
       <ul>
-        
+        {presetItems && setupConfirmation && presetItems().map(name => {
+          return (
+            <li
+              key={name}
+              id={name}
+              onClick={() => setupConfirmation(name)}
+            >{name}
+            </li>
+          )
+        })}
       </ul>
+      {isConfirmationShowing && confirmLoad()}
+      <button onClick={toggleShowMenu}>Cancel</button>
     </div>
   )
 }
