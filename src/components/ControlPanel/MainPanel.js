349a99ddfaf254bc6dba93c718a0f41691aaf582
Silas <silasishallahan@gmail.com>
2022-07-25 07:32:50 -0700

    no borders and shadow

--- src/components/ControlPanel/MainPanel.js
+++ src/components/ControlPanel/MainPanel.js
@@ -17,15 +17,19 @@ export default function MainPanel({
       {isPlaying && <button onClick={() => play(false)}><Stop size={36} /></button>}
       <button onClick={() => randomize()}>Randomize</button>
   
+      <label htmlFor="bpm-controls-container" className="bpm-controls-container-label">Tempo</label>
       <div className='bpm-controls-container'>
         <label htmlFor="speed">{`${displayedBpm} BPM`}</label>
         <input type="range" id="speed" name="speed" min={1} max={400} defaultValue={150}
           onChange={(e) => calculateBpm(e.target.value)} />
       </div>
-      <button id="sequenceLength8" name="sequenceLength8"
-        onClick={() => setSequenceAndNodeStatus(8)}>8</button>
-      <button id="sequenceLength16" name="sequenceLength16"
-        onClick={() => setSequenceAndNodeStatus(16)}>16</button>
+      <label htmlFor="sequence-length-container" className="sequence-length-container-label">Steps</label>
+      <div className="sequence-length-container">
+        <button id="sequenceLength8" name="sequenceLength8"
+          onClick={() => setSequenceAndNodeStatus(8)}>8</button>
+        <button id="sequenceLength16" name="sequenceLength16"
+          onClick={() => setSequenceAndNodeStatus(16)}>16</button>
+      </div>
     </div>
   )
 }
