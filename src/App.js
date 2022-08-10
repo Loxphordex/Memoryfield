c90f0d21fe70524f208ffbe914d6f94f3e42d80a
Silas <silasishallahan@gmail.com>
2022-08-10 13:23:10 -0700

    only kicks

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import playSound from './components/Audio/playSound'
 import { keyShortcuts } from './constants/keyShortcuts'
 import Footer from './components/Footer/Footer'
 
+// samples
+import kick from './samples/KickHollowThud.wav'
+
 // styles
 import "./styles/container.css"
 import "./styles/control.css"
@@ -28,6 +31,17 @@ function App() {
   const [displayedBpm, setDisplayedBpm] = useState(150)
   const [outputLevel] = useState(0.2)
   const [ctx] = useState(new AudioContext())
+
+  const [volume, setVolume] = useState(null)
+  const [osc, setOsc] = useState(null)
+  const [filter, setFilter] = useState(null)
+  const [isSignalSetUp, setIsSignalSetUp] = useState(false)
+  const [isOscStarted, setIsOscStarted] = useState(false)
+
+  const [samplesSetup, setSamplesSetup] = useState(false)
+  const [kickSrc, setKickSrc] = useState(false)
+  const [kickAudio, setKickAudio] = useState(null)
+
   const isPlayingRef = useRef(isPlaying)
   const setPlaying = (status) => {
     isPlayingRef.current = status
@@ -37,12 +51,11 @@ function App() {
   const [defaultKeys, setDefaultKeys] = useState(false)
   const [isKeyHandlerSet, setIsKeyHandlerSet] = useState(false)
 
-  let osc = ctx.createOscillator()
-  let filter = ctx.createBiquadFilter()
-  let volume = ctx.createGain()
-  filter.type = filterTypes.lowpass
+  // filter.type(filterTypes.lowpass)
 
   useEffect(() => {
+    setUpSignalPath()
+    setUpSamples()
     function randomize() {
       setNodes(getRandomSequence(16, nodeSequenceLength))
     }
@@ -74,7 +87,7 @@ function App() {
     let interval
     if (isPlaying) {
       interval = setInterval(() => {
-        if (activeNode >= 0 && nodes != null && isPlaying) playSound(ctx, filter, osc, volume, nodes, activeNode)
+        if (activeNode >= 0 && nodes != null && isPlaying) playSound(ctx, filter, osc, volume, nodes, activeNode, kickAudio)
         if (activeNode >= nodeSequenceLength - 1) {
           // reset sequence
           setActiveNode(0)
@@ -124,6 +137,48 @@ function App() {
     setNodes(getRandomSequence(16, nodeSequenceLength))
   }
 
+  // Set up signal path
+  function setUpSignalPath() {
+    if (isSignalSetUp === false) {
+      setOsc(ctx.createOscillator())
+      setFilter(ctx.createBiquadFilter())
+      setVolume(ctx.createGain())
+      setIsSignalSetUp(true)
+    }
+    
+    if (isOscStarted === false && osc && isSignalSetUp === true) {
+      osc.start()
+      setIsOscStarted(true)
+
+      volume.connect(filter)
+      filter.connect(ctx.destination)
+    }
+  }
+
+  // Set up samples
+  function setUpSamples() {
+    if (samplesSetup === false) {
+      const aud = new Audio(kick)
+      setKickAudio(aud)
+      setSamplesSetup(true)
+    }
+
+    if (samplesSetup === true && kickSrc === false) {
+      const kickSource = ctx.createMediaElementSource(kickAudio)
+      kickSource.connect(ctx.destination)
+      console.log(kickSource)
+      console.log(volume)
+      setKickSrc(true)
+    }
+  }
+
+  // function testKick() {
+  //   const k = new Audio(kick)
+  //   const kSource = ctx.createMediaElementSource(k)
+  //   kSource.connect(volume)
+  //   k.play()
+  // }
+
   return (
     <div className="App">
       <section className="app-container">
@@ -151,6 +206,7 @@ function App() {
           setNodeEditor={setNodeEditor}
         />
       </section>
+      {/* <button onClick={testKick}>Kick</button> */}
       <Footer />
     </div>
   )
--- src/components/Audio/playSound.js
+++ src/components/Audio/playSound.js
@@ -1,32 +1,45 @@
-export default function playSound(ctx, filter, osc, volume, nodes, activeNode) {
+import kick from '../../samples/KickHollowThud.wav'
+
+export default function playSound(ctx, filter, osc, volume, nodes, activeNode, kickAudio) {
   if (ctx) {
-    let now = ctx.currentTime;
-    let currentNode = nodes[activeNode];
-    let freq = currentNode.note.frequency;
-    let wave = currentNode.wave;
-    let end = now + currentNode.endtime;
+    let now = ctx.currentTime
+    let currentNode = nodes[activeNode]
+    let freq = currentNode.note.frequency
+    let wave = currentNode.wave
+    let end = now + currentNode.endtime
 
     // filter setup
-    filter.frequency.setValueAtTime(currentNode.filterFrequency, now);
-    filter.Q.value = currentNode.filterQ;
+    filter.frequency.setValueAtTime(currentNode.filterFrequency, now)
+    filter.Q.value = currentNode.filterQ
 
-    osc.type = wave;
-    osc.start();
-    osc.frequency.value = freq;
-    osc.connect(volume);
-    osc.stop(end);
+
+    const k = new Audio(kick)
+    const kickSource = ctx.createMediaElementSource(k)
+    kickSource.connect(ctx.destination)
+    k.play()
+
+    // osc.type = wave
+    // osc.start()
+    // osc.frequency.value = freq
+    // osc.connect(volume)
+    // osc.stop(end)
+
+    // play sample
+    // const kickAudio = new Audio(kick)
+    // const source = ctx.createMediaElementSource(kickAudio)
+    // source.connect(volume)
+    // kickAudio.play()
+
 
     // envelope
-    volume.gain.cancelScheduledValues(now);
-    volume.gain.setValueAtTime(0.001, now);
-    volume.gain.linearRampToValueAtTime(0.1, now + 0.0008);
-    volume.gain.linearRampToValueAtTime(0, now + 0.1);
+    volume.gain.cancelScheduledValues(now)
+    volume.gain.setValueAtTime(0.001, now)
+    volume.gain.linearRampToValueAtTime(0.1, now + 0.0008)
+    volume.gain.linearRampToValueAtTime(0, now + 0.1)
 
     // Connect nodes
-    // volume.gain.value = outputLevel;
-    volume.connect(filter);
-    filter.connect(ctx.destination);
+    // volume.gain.value = outputLevel
   } else {
-    alert("Sorry, your browser doesn't support JavaScript Web Audio API");
+    alert("Your browser doesn't support JavaScript Web Audio API")
   }
 }
