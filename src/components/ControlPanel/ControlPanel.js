b631e757e9b79e4f67a58a832a04666803c9e919
Silas <silasishallahan@gmail.com>
2022-10-14 14:48:10 -0700

    non active node colors and sample highlights

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -172,6 +172,7 @@ export default function ControlPanel({
             selectSample={selectSample}
             nodeEditor={nodeEditor}
             selectedNode={selectedNode}
+            nodes={nodes}
           />
           {/* <FilterControls
             updateFilterFrequency={updateFilterFrequency}
