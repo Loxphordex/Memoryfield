9120ef35b4de930dd53468818d0cf621fc0f3ddc
Silas <silasishallahan@gmail.com>
2022-11-28 17:06:52 -0800

    set load preset menu and confirmation box side by side

--- src/components/Presets/LoadPreset/LoadPresetMenu.js
+++ src/components/Presets/LoadPreset/LoadPresetMenu.js
@@ -6,24 +6,29 @@ export default function LoadPresetMenu({
   setupConfirmation,
   confirmLoad
 }) {
+
   return (
     <div className='load-preset-menu'>
-      <label className='display-panel-label' htmlFor='load-preset-list'>Load Preset</label>
-      <ul id='load-preset-list' className='preset-list sequence-list'>
-        {presetItems && setupConfirmation && presetItems().map(name => {
-          return (
-            <div key={name} className='load-preset-list-item-container'>
-              <li
-                id={name}
-                className='preset-list-item display-panel-list-item-highlight'
-                onClick={() => setupConfirmation(name)}
-              >{name}
-              </li>
-            </div>
-          )
-        })}
-      </ul>
-      {isConfirmationShowing && confirmLoad()}
+      <div className='load-preset-list-container'>
+        <label className='display-panel-label' htmlFor='load-preset-list'>Load Preset</label>
+        <ul id='load-preset-list' className='preset-list sequence-list'>
+          {presetItems && setupConfirmation && presetItems().map(name => {
+            return (
+              <div key={name} className='load-preset-list-item-container'>
+                <li
+                  id={name}
+                  className='preset-list-item display-panel-list-item-highlight'
+                  onClick={() => setupConfirmation(name)}
+                >{name}
+                </li>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
+      <div className='confirm-load-container'>
+        {isConfirmationShowing && confirmLoad()}
+      </div>
     </div>
   )
 }
